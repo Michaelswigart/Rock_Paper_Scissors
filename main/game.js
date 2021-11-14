5dcf8799c1c8ce9cebc90b53c60f503b94cd12d2
michael swigat <sherlockholmes.swigart92@gmail.com>
2021-11-14 11:43:22 -0600

    I finle did some Javascript for this

--- main/game.js
+++ main/game.js
@@ -1,1 +1,27 @@
-// I want to make a simple javescipt game wiht rock paper scissors game and use img tags for buttons need to find out how to do so. first step for javascript is to make the rules for the game. step 2 is to try to use img tags to for buttons.  And try to have fun with this\[T]/ -_-.
+// I want to make a simple javescipt game wiht rock paper scissors game. first step for javascript is to make the rules for the game. step 2 is to try to use img tags to for buttons.  And try to have fun with this\[T]/ -_-.
+
+
+// good first part of javascript for me.
+function game (x, y, z) {
+    let x = rock;
+    let y = paper;
+    let z = scissors;
+  if (x > z) {
+      alert = "that hurt"
+  }
+   else (x < y); {
+     alert = "wrong"
+  }
+   if (z < y) {
+    alert = "that hurt"
+  }
+   else (z > x); {
+    alert = "wrong"
+  }
+   if (y > x) {
+    alert = "that hurt"
+  }
+   else (y < z); {
+    alert = "that hurt"
+  }
+};
