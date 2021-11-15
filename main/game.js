8d87ffe04e2ebfccf570bbba698c8162d1a15b09
michael swigat <sherlockholmes.swigart92@gmail.com>
2021-11-14 19:23:17 -0600

    update to javascript

--- main/game.js
+++ main/game.js
@@ -1,6 +1,18 @@
 // I want to make a simple javescipt game wiht rock paper scissors game. first step for javascript is to make the rules for the game. step 2 is to try to use img tags to for buttons.  And try to have fun with this\[T]/ -_-.
 
 
+// let playGame = confirm("shall we play rock, paper or scissors")
+
+// if (playGame) {
+//   //play
+//   let playerChoice = promt ('Please enter rock, paper, or scissors');
+//   if (playerChoice) {
+
+//   }
+// } else {
+//   alert('ok, maybe later');
+// }
+
 // good first part of javascript for me.
 function game (x, y, z) {
     let x = rock;
