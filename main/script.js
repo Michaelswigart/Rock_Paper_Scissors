22c067246d57c158be045ef6b14791883f0c2f3a
michael swigat <sherlockholmes.swigart92@gmail.com>
2021-11-17 23:05:31 -0600

    I fixed the javascript and made it right

--- main/script.js
+++ main/script.js
@@ -1,39 +1,30 @@
-// I want to make a simple javescipt game wiht rock paper scissors game. first step for javascript is to make the rules for the game. step 2 is to try to use img tags to for buttons.  And try to have fun with this\[T]/ -_-.
+const selectionButtons = document.querySelectorAll('[data-selection]')
+const SELECTIONS = [
+{
+  name: 'rock',
+  emoji: '✊',
+  beats: 'scissors'
+},{
+  name: 'paper',
+  emoji: '✋',
+  beats: 'rock'
+},{
+  name: 'scissors',
+  emoji: '✌️',
+  beats: 'paper'
+},
+] 
+
+selectionButtons.forEach(selectionButton => {
+  selectionButton.addEventListener('click', e  => {    
+   const selectionName = selectionButton.dataset.selection
+   const selection = SELECTIONS.find(selection => selection.name === selectionName)
+    makeSelection(selection)
+  })
+})
+
+function makeSelection(selection) {
+  console.log(selection)
+}
 
 
-// let playGame = confirm("shall we play rock, paper or scissors")
-
-// if (playGame) {
-//   //play
-//   let playerChoice = promt ('Please enter rock, paper, or scissors');
-//   if (playerChoice) {
-
-//   }
-// } else {
-//   alert('ok, maybe later');
-// }
-
-// good first part of javascript for me.
-function game (x, y, z) {
-    let x = rock;
-    let y = paper;
-    let z = scissors;
-  if (x > z) {
-      alert = "that hurt"
-  }
-   else (x < y); {
-     alert = "wrong"
-  }
-   if (z < y) {
-    alert = "that hurt"
-  }
-   else (z > x); {
-    alert = "wrong"
-  }
-   if (y > x) {
-    alert = "that hurt"
-  }
-   else (y < z); {
-    alert = "that hurt"
-  }
-};
